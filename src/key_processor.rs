e23812f0e42a77bdb793d35fa0be2d896fa15f4b
David Horvát <horvatda@proton.me>
2023-12-12 18:38:12 +0100

    added argon key derive

--- src/key_processor.rs
+++ src/key_processor.rs
@@ -1,5 +1,27 @@
 use shielded::Shielded;
+use argon2::Argon2;
 
+
+static SALT: &[u8; 14] = b"cru5tw0rd5a1ty";
+
+
+// insert clear text key and return a vault
+pub fn process_input_key(password: String) -> SecureStorage {
+    let expand = derive_key(password);
+    SecureStorage::new(expand)
+}
+
+fn derive_key(password: String) -> Vec<u8> {
+    let mut key = [0u8; 64];
+
+    Argon2::default().hash_password_into(
+        password.as_ref(),
+        SALT,
+        &mut key,
+    ).unwrap();
+
+    key.to_vec()
+}
 
 struct SecureStorage {
     memory: Shielded,
