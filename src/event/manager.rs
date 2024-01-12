e206b4f30f3529f33bffe4685a7dd1338a070961
David Horvát <horvatda@proton.me>
2024-01-12 10:57:18 +0100

    improved keybinds

--- src/event/manager.rs
+++ src/event/manager.rs
@@ -12,66 +12,75 @@ pub fn handle_events(app: &mut App) -> Result<ControlFlow<()>, Box<dyn Error>> {
     if let Event::Key(key) = event::read()? {
         // match inputs depending on currently displayed page
         // default events
-        match key.code {
-            KeyCode::Tab => app.page_index.page_up(),
-            KeyCode::BackTab => app.page_index.page_down(),
+        // page specific events
+        match app.page_index.index {
+            0 => {
+                match key.code {
+                    KeyCode::Esc => { return Ok(ControlFlow::Break(())); }
 
-            _ => {
-                // page specific events
-                match app.page_index.index {
-                    0 => {
-                        match key.code {
-                            KeyCode::Esc => { return Ok(ControlFlow::Break(())); }
+                    KeyCode::Tab => app.page_index.page_up(),
+                    KeyCode::BackTab => app.page_index.page_down(),
 
-                            KeyCode::Up => app.entries_list.previous(),
-                            KeyCode::Down => app.entries_list.next(),
+                    KeyCode::Up => app.entries_list.previous(),
+                    KeyCode::Down => app.entries_list.next(),
 
-                            _ => { app.text_fields.search_bar.input(key); }
-                        }
-                    }
-                    1 => {
-                        match app.page_selected {
-                            false => match key.code {
-                                KeyCode::Esc => { return Ok(ControlFlow::Break(())); }
-
-                                KeyCode::Up => app.template_names.previous(),
-                                KeyCode::Down => app.template_names.next(),
-
-                                KeyCode::Right => app.select_template(),
-                                KeyCode::Enter => app.reset_input_fields(),
-
-                                _ => {}
-                            }
-                            true => match key.code {
-                                KeyCode::Esc => { app.unselect_right(); }
-
-                                KeyCode::Up => { app.text_fields.edit_fields.as_mut().unwrap().previous(); }
-                                KeyCode::Down => { app.text_fields.edit_fields.as_mut().unwrap().next(); }
-                                KeyCode::Enter => {
-                                    let fields = app.text_fields.edit_fields.as_ref().unwrap();
-                                    if let Some(index) = fields.current() {
-                                        if index == fields.items.len() - 1 {
-                                            app.save_entry();
-                                        } else {
-                                            app.text_fields.edit_fields.as_mut().unwrap().next();
-                                        }
-                                    }
-                                }
-                                _ => {
-                                    let fields = app.text_fields.edit_fields.as_mut().unwrap();
-                                    if let Some(index) = fields.current() {
-                                        if index != fields.items.len() - 1 {
-                                            app.text_fields.edit_fields.as_mut().unwrap().items[index].input(key);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    2 => {}
-                    _ => unreachable!()
+                    _ => { app.text_fields.search_bar.input(key); }
                 }
             }
+            1 => {
+                match app.page_selected {
+                    false => match key.code {
+                        KeyCode::Esc => { return Ok(ControlFlow::Break(())); }
+
+                        KeyCode::Tab => app.page_index.page_up(),
+                        KeyCode::BackTab => app.page_index.page_down(),
+
+                        KeyCode::Up => app.template_names.previous(),
+                        KeyCode::Down => app.template_names.next(),
+
+                        KeyCode::Right => app.select_template(),
+                        KeyCode::Enter => app.reset_input_fields(),
+
+                        _ => {}
+                    }
+                    true => match key.code {
+                        KeyCode::Esc => { app.unselect_right(); }
+
+                        KeyCode::Up | KeyCode::BackTab => { app.text_fields.edit_fields.as_mut().unwrap().previous(); }
+                        KeyCode::Down | KeyCode::Tab => { app.text_fields.edit_fields.as_mut().unwrap().next(); }
+
+                        KeyCode::Enter => {
+                            let fields = app.text_fields.edit_fields.as_ref().unwrap();
+                            if let Some(index) = fields.current() {
+                                if index == fields.items.len() - 1 {
+                                    app.save_entry();
+                                } else {
+                                    app.text_fields.edit_fields.as_mut().unwrap().next();
+                                }
+                            }
+                        }
+                        _ => {
+                            let fields = app.text_fields.edit_fields.as_mut().unwrap();
+                            if let Some(index) = fields.current() {
+                                if index != fields.items.len() - 1 {
+                                    app.text_fields.edit_fields.as_mut().unwrap().items[index].input(key);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            2 => {
+                match key.code {
+                    KeyCode::Esc => { return Ok(ControlFlow::Break(())); }
+
+                    KeyCode::Tab => app.page_index.page_up(),
+                    KeyCode::BackTab => app.page_index.page_down(),
+
+                    _ => {}
+                }
+            }
+            _ => unreachable!()
         }
     }
 
