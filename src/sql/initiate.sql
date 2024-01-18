63ef82fa421d14e52c4f6a121437ee8844c2e51e
David Horvát <horvatda@proton.me>
2024-01-18 22:54:19 +0100

    added bank option

--- src/sql/initiate.sql
+++ src/sql/initiate.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS templates
 );
 
 -- username-password template
-CREATE TABLE IF NOT EXISTS "dHBfc2ltcGxl"
+CREATE TABLE IF NOT EXISTS "dHBfd2ViX2NyZWRlbnRpYWw="
 (
     description TEXT UNIQUE,
     clear_1     TEXT,
@@ -65,11 +65,43 @@ VALUES ('SSH-Keypair', CAST('{
   ]
 }' AS BLOB));
 
+-- banking information
+CREATE TABLE IF NOT EXISTS "dHBfc3NoX2tleXBhaXI="
+(
+    description TEXT UNIQUE,
+    clear_1     TEXT,
+    hidden_1    Text,
+    hidden_2    Text
+);
+INSERT INTO templates (name, structure)
+VALUES ('Banking', CAST('{
+  "deletable": false,
+  "name": "Banking",
+  "elements": [
+    {
+      "name": "Name",
+      "private": false
+    },
+    {
+      "name": "Bank",
+      "private": false
+    },
+    {
+      "name": "IBAN",
+      "private": true
+    },
+    {
+      "name": "Kontonummer",
+      "private": true
+    }
+  ]
+}' AS BLOB));
+
 CREATE TABLE IF NOT EXISTS nonces
 (
-    nonce TEXT UNIQUE,
+    nonce      TEXT UNIQUE,
     orig_table TEXT,
-    orig_desc TEXT,
+    orig_desc  TEXT,
     orig_entry TEXT
 );
 
