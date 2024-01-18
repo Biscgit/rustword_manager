f5a033e01a5d9ce211a2a1c6c7f4bb14fa65ff3b
David Horvát <horvatda@proton.me>
2024-01-18 14:44:22 +0100

    created panic hook for ratatui

--- src/terminal.rs
+++ src/terminal.rs
@@ -9,6 +9,8 @@ use crate::types::Terminal;
 
 pub fn setup_terminal() -> Result<Terminal, Box<dyn Error>> {
     // helper method to setup terminal. See Ratatui Manuals
+    initialize_panic_handler();
+
     enable_raw_mode()?;
     let mut stdout = stdout();
     execute!(stdout, EnterAlternateScreen)?;
@@ -22,4 +24,13 @@ pub fn restore_terminal(mut terminal: Terminal) -> Result<(), Box<dyn Error>> {
     disable_raw_mode()?;
     execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
     Ok(())
+}
+
+pub fn initialize_panic_handler() {
+    let original_hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |panic_info| {
+        execute!(std::io::stderr(), LeaveAlternateScreen).unwrap();
+        disable_raw_mode().unwrap();
+        original_hook(panic_info);
+    }));
 }
